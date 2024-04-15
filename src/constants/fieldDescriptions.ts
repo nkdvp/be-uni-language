968de2f54d70c968f947e283611ad255c295f206
DucNK <ducnk@mpos.vn>
2024-04-15 21:28:53 +0700

    Add search by view count

--- src/constants/fieldDescriptions.ts
+++ src/constants/fieldDescriptions.ts
@@ -14,6 +14,10 @@ const newsFields: FieldDescription[] = [
     field: 'group',
     dataType: DataType.STRING,
   },
+  {
+    field: 'viewCount',
+    dataType: DataType.INTEGER,
+  },
 ];
 
 export { newsFields };
